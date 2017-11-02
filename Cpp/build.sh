ad51911dbb1078dc42c13a5b7acd0ed3d8cb664f
Tiggles <jonas@hinchely.dk>
2017-11-02 19:35:07 +0100

    Updated gitignore

--- Cpp/build.sh
+++ Cpp/build.sh
@@ -1,3 +1,3 @@
 #g++ -std=c++14 -lSDL2main -lSDL2 Raytracer.cpp
 
-g++ Raycaster.cpp $(pkg-config --cflags --libs sdl2) -o Raytracer
+g++ Raycaster.cpp $(pkg-config --cflags --libs sdl2) -o Raycaster
