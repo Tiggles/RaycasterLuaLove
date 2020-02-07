640f07111c48825ba53b1cce935fbc7ba8dd6e6b
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-02-07 14:30:12 +0100

    Render something

--- Rust/raycaster/src/main.rs
+++ Rust/raycaster/src/main.rs
@@ -1,6 +1,11 @@
 extern crate sdl2;
+
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use std::time::Duration;
 
 struct Vector2D {
     x: f64,
@@ -19,13 +24,26 @@ struct Player {
     rot_speed: f64,
 }
 
-const SCREEN_HEIGHT: u16 = 600;
-const SCREEN_WIDTH: u16 = 800;
+const SCREEN_HEIGHT: u32 = 600;
+const SCREEN_WIDTH: u32 = 800;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let worldMap: Vec<Vec<u8>> = vec![
+
+    let window = video_subsystem
+        .window("Raycaster", SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let world_map: Vec<Vec<u8>> = vec![
         vec![
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
         ],
@@ -132,16 +150,33 @@ fn main() {
         let delta_time = 0;
         let last = 0;
         running = handle_input(&player, &sdl_context, 0);
+        canvas = render(canvas);
     }
 
     println!("Exiting");
+}
+
+fn render(mut canvas: Canvas<Window>) -> Canvas<Window> {
+    use sdl2::rect::Point;
+    let mut iteration: u8 = 0;
+    for i in 0..SCREEN_WIDTH {
+        iteration = (iteration + 1) % 255;
+        canvas.set_draw_color(Color::RGB(iteration as u8, 64, 255 - iteration));
+        canvas.draw_line(
+            Point::new(i as i32, 0),
+            Point::new(i as i32, SCREEN_HEIGHT as i32),
+        );
+    }
+    canvas.present();
+    canvas
 }
 
 fn handle_input(player: &Player, sdl_context: &sdl2::Sdl, delta_time: u128) -> bool {
     let mut event_pump = sdl_context.event_pump().unwrap();
     for event in event_pump.poll_iter() {
         match event {
-            Event::Quit { .. } | Event::KeyDown {
+            Event::Quit { .. }
+            | Event::KeyDown {
                 keycode: Some(Keycode::Escape),
                 ..
             } => return false,
