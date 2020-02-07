01b66e22d1c5d06fb9ad13bb10ff6d90c8336ee4
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-02-07 19:11:17 +0100

    Better keypress checks

--- Rust/raycaster/src/main.rs
+++ Rust/raycaster/src/main.rs
@@ -3,7 +3,7 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::render::Canvas;
+use sdl2::render::{Canvas, WindowCanvas};
 use sdl2::video::Window;
 use std::time::Duration;
 
@@ -29,11 +29,13 @@ const SCREEN_WIDTH: u32 = 800;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    let video_subsystem = sdl_context.video().expect("Failed getting video subsystem");
 
     let window = video_subsystem
         .window("Raycaster", SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32)
         .position_centered()
+        .resizable()
+        .allow_highdpi()
         .build()
         .unwrap();
 
@@ -133,7 +135,7 @@ fn main() {
         y: 0.66f64,
     };
 
-    let player = Player {
+    let mut player = Player {
         direction,
         position,
         plane,
@@ -145,46 +147,74 @@ fn main() {
         rot_speed: 0.04f64,
     };
 
-    let mut running = true;
-    while running {
+    loop {
         let delta_time = 0;
         let last = 0;
-        running = handle_input(&player, &sdl_context, 0);
+        player = match handle_input(player, &sdl_context) {
+            Ok(p) => p,
+            Err(_) => break,
+        };
         canvas = render(canvas);
     }
 
     println!("Exiting");
 }
 
-fn render(mut canvas: Canvas<Window>) -> Canvas<Window> {
+fn render(mut canvas: WindowCanvas) -> WindowCanvas {
     use sdl2::rect::Point;
     let mut iteration: u8 = 0;
     for i in 0..SCREEN_WIDTH {
         iteration = (iteration + 1) % 255;
         canvas.set_draw_color(Color::RGB(iteration as u8, 64, 255 - iteration));
-        canvas.draw_line(
-            Point::new(i as i32, 0),
-            Point::new(i as i32, SCREEN_HEIGHT as i32),
-        );
+        canvas
+            .draw_line(
+                Point::new(i as i32, 0),
+                Point::new(i as i32, SCREEN_HEIGHT as i32),
+            )
+            .expect("Something went wrong drawing vertical lines");
     }
     canvas.present();
     canvas
 }
 
-fn handle_input(player: &Player, sdl_context: &sdl2::Sdl, delta_time: u128) -> bool {
+fn handle_input(mut player: Player, sdl_context: &sdl2::Sdl) -> Result<Player, bool> {
     let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut exit = false;
     for event in event_pump.poll_iter() {
         match event {
             Event::Quit { .. }
             | Event::KeyDown {
                 keycode: Some(Keycode::Escape),
                 ..
-            } => return false,
+            } => exit = true,
             Event::KeyDown {
                 keycode: Some(key), ..
-            } => println!("{}", key),
+            } => match key {
+                Keycode::Up => player.up = true,
+                Keycode::Down => player.down = true,
+                Keycode::Left => player.left = true,
+                Keycode::Right => player.right = true,
+                _ => println!("Pressed {}", key),
+            },
+            Event::KeyUp {
+                keycode: Some(key), ..
+            } => match key {
+                Keycode::Up => player.up = false,
+                Keycode::Down => player.down = false,
+                Keycode::Left => player.left = false,
+                Keycode::Right => player.right = false,
+                _ => println!("Released {}", key),
+            },
             _ => {}
         }
     }
-    true
+    println!("{}", player.up);
+    println!("{}", player.down);
+    println!("{}", player.left);
+    println!("{}", player.right);
+    if exit {
+        Err(true)
+    } else {
+        Ok(player)
+    }
 }
